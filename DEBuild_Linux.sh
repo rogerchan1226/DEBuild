656ffb5fc2d4ada028ec2e1f9771354224b4a897
roger_chan <roger_chan@calcomp.com>
2019-08-08 11:38:03 +0800

    [Beta] DEBuild update to Beta v3.0
    Update message
        Update
        -- Add auto detect linux platform functions at DEBuild_Linux.sh
        -- Create setup.sh at path "/src" to list up packages
        -- Rename "setup.mk" to "install.mk"

--- DEBuild_Linux.sh
+++ DEBuild_Linux.sh
@@ -17,40 +17,108 @@ printf "\e[0m"
 
 # Show logo and DEBuild version
 printf "\e[32m"
-cat src/version
+    cat src/version
 printf "\e[0m"
 
+# Ask user if start install packages or not
+echo    "Do you want to install following packages?   "
+    source src/setup.sh
+echo -n "Answer [Y/n]: "
+
+install_yn () {
+    read answer
+    case $answer in
+        [Yy] | [Yy][Ee][Ss] )
+            echo "Start install... "
+            ;;
+        [Nn] | [Nn][Oo] )
+            echo "End !"
+            exit -1
+            ;;
+        * )
+            printf "\e[37;41m"
+                echo -n "ERROR!" 
+            printf "\e[0m"
+            echo    " Sorry, answer not recognized"
+            echo -n "Please answer again... [Y/n]: "
+            install_yn
+            ;;
+    esac
+}
+
+install_yn
+
 # Choose the linux platform 
-echo    "Which Linux platform you are using now?"
-echo    "   1. Debian-based (Debian, Ubuntu, ...etc)"
-echo    "   2. Fedora-based (Fedora, CentOS, ...etc)"
-echo -n "Please select the option as above [1/2]: "
-read selection
+platform_select () {
+    case $selection in
+        1 )
+            PKG_MANAGER=apt-get
+            echo "PKG_MANAGER=apt-get" > src/settings.conf
+            ;;
+        2 )
+            PKG_MANAGER=yum
+            echo "PKG_MANAGER=yum" > src/settings.conf
+            ;;
+        * )
+            printf "\e[37;41m"
+                echo -n "ERROR!" 
+            printf "\e[0m"
+            echo -n " 1 or 2 only, please select again [1/2]: "
+            input_pselect
+            ;;
+    esac
+}
 
-while [ $selection != "1" ] && [ $selection != "2" ]; do
-    printf "\e[37;41m"
-    echo -n "ERROR!" 
-    printf "\e[0m"
-    echo -n " 1 or 2 only, please select again [1/2]: "
+input_pselect () {
     read selection
-done
+    platform_select
+}
 
-if [ $selection = "1" ]; then
-    PKG_MANAGER=apt-get
-    echo "PKG_MANAGER=apt-get" > src/settings.conf
+platform () {
+    echo    "Which Linux platform you are using now?    "
+    echo    "   1. Debian-based (Debian, Ubuntu, ...etc)"
+    echo    "   2. Fedora-based (Fedora, CentOS, ...etc)"
+    echo -n "Please select the option as above [1/2]: "
+    input_pselect
+}
+
+echo "Detect platform Name..."
+
+if [ -f /etc/\*-release ]; then
+    echo "Platform detect FAIL!!!"
+    platform
 else
-    PKG_MANAGER=yum
-    echo "PKG_MANAGER=yum" > src/settings.conf
+    # String processing
+    platform_value=`cat /etc/*-release | grep 'ID_LIKE=' | cut -d"=" -f 2`
+    ## echo $platform_value
+    case $platform_value in
+        debian )
+            printf "\e[37;42m"
+                echo -n "PASS" 
+            printf "\e[0m"
+            echo " Oh~ you are Debian-based user huh.."
+            selection=1
+            platform_select
+            ;;
+        Fedora )
+            printf "\e[37;42m"
+                echo -n "PASS" 
+            printf "\e[0m"
+            echo " Oh~ you are Fedora-based user huh.."
+            selection=2
+            platform_select
+            ;;
+        * )
+            echo "Platform detect FAIL!!!"
+            platform
+            ;;
+    esac
 fi
 
-# Several config settings setup to "settings.conf"
-source src/config
 
-if [ $CONFIG_IDE_SUBL_STABLE_INSTALL = "y" ] && [ $CONFIG_IDE_SUBL_DEV_INSTALL = "y" ]; then
-    printf "\e[31m"
-        echo "Sublime install fail, can't turn on Stable/DEV both, Select one only."
-    printf "\e[0m"
-elif [ $CONFIG_IDE_SUBL_STABLE_INSTALL = "y" ] || [ $CONFIG_IDE_SUBL_DEV_INSTALL = "y" ]; then
+
+# Several config settings setup to "settings.conf"
+if [ $CONFIG_IDE_SUBL_STABLE_INSTALL = "y" ] || [ $CONFIG_IDE_SUBL_DEV_INSTALL = "y" ]; then
     if [ $CONFIG_IDE_SUBL_STABLE_INSTALL = "y" ]; then
         SUBL_VER=stable
     elif [ $CONFIG_IDE_SUBL_DEV_INSTALL = "y" ]; then
@@ -72,5 +140,5 @@ fi
 
 # Start install environment
 pushd src/ || exit -1
-make -f setup.mk
+    make -f install.mk
 popd
---
+++ src/setup.sh
@@ -0,0 +1,53 @@
+source src/config
+
+
+# Check out if there got wrong settings
+if [ $CONFIG_IDE_SUBL_STABLE_INSTALL = "y" ] && [ $CONFIG_IDE_SUBL_DEV_INSTALL = "y" ]; then
+    printf "\e[37;41m"
+        echo -n "ERROR!" 
+    printf "\e[0m"
+    echo " Sublime install fail, can't turn on Stable/DEV both, Select one only."
+	exit -1
+fi
+
+total_pkgs=0
+
+# Install packages log pre process.
+	echo " __________________________"
+	echo "|                          |"
+if [ $CONFIG_GIT_INSTALL = "y" ]; then
+	echo "| Git                      |"
+	total_pkgs=$((total_pkgs + 1))
+fi
+
+if [ $CONFIG_INSTALL_C_DEBuild = "y" ]; then
+	echo "| Build-essential          |"
+	total_pkgs=$((total_pkgs + 1))
+fi
+
+if [ $CONFIG_INSTALL_CLIB = "y" ]; then
+	echo "| Several C Library        |"
+	total_pkgs=$((total_pkgs + 1))
+fi
+
+if [ $CONFIG_IDE_SUBL_STABLE_INSTALL = "y" ]; then
+	echo "| Sublime stable version   |"
+	total_pkgs=$((total_pkgs + 1))
+fi
+
+if [ $CONFIG_IDE_SUBL_DEV_INSTALL = "y" ]; then
+	echo "| Sublime develop version   |"
+	total_pkgs=$((total_pkgs + 1))
+fi
+
+if [ $total_pkgs = "0" ]; then
+	echo "| NONE                     |"
+	echo "|__________________________|"
+	printf "\e[37;41m"
+        echo -n "ERROR!" 
+    printf "\e[0m"
+	echo " There is no packages will be install, please checkout \"src/config\" again."
+	exit -1
+else
+	echo "|__________________________|"
+fi
